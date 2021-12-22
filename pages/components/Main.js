c5c6463b0584acc78ba165f9ce38d18bfddc70df
rihan <Rihanfoudeh@gmail.com>
2021-12-23 01:11:44 +0200

    solve lab39

--- pages/components/Main.js
+++ pages/components/Main.js
@@ -1,38 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import workingHours from './data'
 import CreateForm from './CreateForm.js';
 import ReportTable from './ReportTable.js';
+import axios from "axios";
+
+
 import Footer from './Footer'
 
-export default function Main() {
 
-    // const [workingHours, setworkingHours] = useState(['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'])
-    
-    
+const baseUrl = 'https://cookie-stand-api-rihan.herokuapp.com/';
+const responsesEndPoint = baseUrl + 'api/v1/cookie_stands/';
+
+export default function Main(props) {
+
     const [report, setallstores] = useState([])
-    
+
     const [totals, setbranchestotals] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
 
+    useEffect(() => {
+        if (props.token) {
+            getRepliesFromAPI();
+        }
+    }, [props.token]);
+
+    let allData = []
+    const getRepliesFromAPI = async () => {
+        console.log("hiii");
+
+        const config = { headers: { 'Authorization': 'Bearer ' + props.token } };
+        const answers = await axios.get(responsesEndPoint, config);
+        console.log("hiiiia", answers.data);
+
+        const mappedData = answers.data.map((item) => {
+            const storeData = {
+                id: item.id,
+                location: item.location,
+                average_cookies_per_sale: item.average_cookies_per_sale,
+                minimum_customers_per_hour: item.minimum_customers_per_hour,
+                maximum_customers_per_hour: item.maximum_customers_per_hour,
+                hourlySales: item.hourly_sales,
+
+            }
+            allData.push(storeData)
+            setallstores([...report, ...allData]);
+            return storeData
+        });
+    }
+   
     function onCreate(event) {
-
         event.preventDefault();
-
         let minCustomers = parseInt(event.target.minCustomers.value)
         let maxCustomers = parseInt(event.target.maxCustomers.value)
         let avgCookies = parseInt(event.target.avgCookies.value)
-
         let sum = 0
-
-
         const store = {
             location: event.target.location.value,
             hourlySales: workingHours.map(() => Math.ceil(avgCookies * (Math.ceil(Math.random() * (maxCustomers - minCustomers) + minCustomers)))),
         }
-
-
         for (let i = 0; i < workingHours.length; i++) {
-            sum +=  store.hourlySales[i]
+            sum += store.hourlySales[i]
         }
         store.total = sum
 
@@ -40,27 +67,62 @@ export default function Main() {
 
 
         let total_sum = totals.map((item, idx) => {
-            if (idx === totals.length -1) {
+            if (idx === totals.length - 1) {
                 return item + store.total
             }
             return item + store.hourlySales[idx]
+
+            
         })
+        const configPost = {
+            method: "POST",
+            url: responsesEndPoint,
+            headers: { "Authorization": `Bearer ${props.token}` },
+            data:  {
+                location: event.target.location.value,
+                description: '',
+                hourly_sales: store.hourlySales,
+                minimum_customers_per_hour: minCustomers,
+                maximum_customers_per_hour: maxCustomers,
+                average_cookies_per_sale: avgCookies,
+                owner: 1
+            }
+        }
+    
+        axios(configPost)
 
         setbranchestotals(total_sum)
 
+
+
+
+    };
+
+
+
+    const deleteHandler =async (id) => {
+        const config = {
+            method: "DELETE",
+            url: `https://cookie-stand-api-rihan.herokuapp.com/api/v1/cookie_stands/${id}`,
+            headers: { "Authorization": `Bearer ${props.token}` }
+
+
+
+        }
+
+       
+        await axios(config)
     }
+return (
+    <>
+        <main className="">
 
-    return (
-        <>
-            <main className="">
-                
-                <CreateForm onCreate={onCreate} />
+            <CreateForm onCreate={onCreate} />
 
-                <ReportTable report={report} workingHours={workingHours} totals={totals} />
+            <ReportTable report={report} workingHours={workingHours} totals={totals} deleteHandler={deleteHandler} />
+            <Footer report={report} />
 
-                <Footer report={report} />
-
-            </main>
-        </>
-    )
+        </main>
+    </>
+)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,59 @@
 import Head from 'next/head'
+import { useState } from 'react';
+
 
 import Header from './components/Header'
 import Main from './components/Main'
+import LoginForm from './components/LoginForm'
+import axios from 'axios';
+
 // import Footer from '../components/Footer'
+
+
+const baseUrl ='https://cookie-stand-api-rihan.herokuapp.com/';
+const tokenUrl = baseUrl+'api/token/';
 
 export default function CookieStandAdmin() {
 
-  return (
-    <div className="bg-green-50">
+  const [token, setToken] = useState('');
+
+  const submitHandler = async (e, credintials)=>{
+    e.preventDefault();
+    axios.post(tokenUrl,credintials).then(data=>{
+      setToken(data.data.access)
+    });
+    console.log(token)
+  }
+  
+
+  // return (
+    // <div className="bg-green-50">
       
-      <Head>
-        <title>Cookie Stand Admin</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    //   <Head>
+    //     <title>Cookie Stand Admin</title>
+    //     <link rel="icon" href="/favicon.ico" />
+    //   </Head>
 
-      <Header />
+      if (!token)  return <LoginForm submitHandler={submitHandler}/> 
+      return (
+       <> 
+       <Header/>
+      <Main token={token}/>
+      </>
+      )
+    //   <Main />
 
-      <Main />
+    //   {/* <Footer/> */}
 
-      {/* <Footer/> */}
-
-    </div>
-  )
+    // </div>
+  // )
 }
+
+
+
+
+
+
+
+
+
